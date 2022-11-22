c869c41a70b38eb1d68d97ec5525f3574071c5d4
SashaPavlovski <sasha123.ez@gmailcom>
2022-11-22 22:23:19 +0200

    hashtable

---
+++ iceCream.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashtable_CS
+{
+    internal class IceCream
+    {
+        public IceCream(int ID)
+        {
+            _Id = ID;
+        }
+        private readonly int _Id;
+
+        public int Id
+        {
+            get { return _Id; }
+
+        }
+
+        public int numIceCreamBall { get; set; }
+
+    }
+}
---
+++ manager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashtable_CS
+{
+    internal class Manager
+    {
+
+        private Manager() { }
+    
+
+        private readonly static Manager _INSTANCE = new Manager();
+
+        public static Manager INSTANCE
+        {
+            get { return _INSTANCE; }
+
+        }
+
+        Hashtable tbl = new Hashtable();
+
+        public void LoadTable()
+        {
+
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 1; i < 1000; i++)
+            {
+                int randomNum = random.Next(1, 5);
+                if (randomNum % 2 == 0)
+                {
+                    Falafel yossi = new Falafel(randomNum);
+                    yossi.numfalafelballs = random.Next(100, 10000);
+                    object o = tbl[yossi.Id];
+                    Falafel num = (Falafel)o;
+                    if (!(tbl[yossi.Id] is Falafel))
+                    {
+                        tbl.Add(yossi.Id, yossi);
+                    }
+                    else
+                    {
+                        Console.WriteLine("the id already exsit");
+                    }
+
+                }
+                else
+                {
+                    IceCream Golda = new IceCream(randomNum);
+                    Golda.numIceCreamBall = random.Next(1, 5);
+
+
+                    if (!(tbl[Golda.Id] is IceCream))
+                    {
+                        tbl.Add(Golda.Id, Golda);
+                    }
+                    else
+                    {
+                        Console.WriteLine("the id already exsit");
+                    }
+
+                }
+
+
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("please enter a number between 100 to 9999");
+                string numUser = Console.ReadLine();
+                int userResponse = int.Parse(numUser);
+
+                if (tbl[userResponse] is Falafel)
+                {
+                    Console.WriteLine($"this falafel has {((Falafel)tbl[userResponse]).numfalafelballs}");
+                }
+                else if (tbl[userResponse] is IceCream)
+                {
+                    Console.WriteLine($"this iceCream has {((IceCream)tbl[userResponse]).numIceCreamBall}");
+                }
+                else
+                {
+                    Console.WriteLine("the number dosent exsit in the table");
+                }
+            }
+        }
+
+    }
+}
